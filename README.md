84ebd94c301a8a915e8c528d7129b5cb7fb5f71d
bhagyeshagresar <bhagyeshagresar@gmail.com>
2025-10-28 22:27:53 -0500

    added readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Design Principles in C++
+
+Section 1: SOLID Principles
+    1) Single Responsibility Principle
+    2) Open-Closed Principle
+    3) Liskov Substitution Principle
+    4) Interface Segregation Principle
+    5) Dependency Inversion Principle
--- liskov_substitution_principle.cpp
+++ liskov_substitution_principle.cpp
@@ -12,15 +12,53 @@ class Rectangle
             return width;
         }
 
-        void setWidth(int width){
+        virtual void setWidth(int width){
             Rectangle::width = width;
         }
 
+        // Const member function (accessor)
+        // Can be called on both const and non-const objects.
+        // The 'const' keyword after the signature ensures this function
+        // does not change 'height' or 'width'.
         int getHeight() const{
             return height;
         }
 
-        void setHeight(int height){
+        virtual void setHeight(int height){
             Rectangle::height = height;
         }
 };
+
+//This class below violates the liskov substitution principle. The process function does not work for derived classes like square(gives unexpected results)
+class Square: public Rectangle
+{
+    public:
+        Square(int size): Rectangle(size, size) {}
+
+        void setWidth(int width) override{
+            this->width  = this->height = width;
+        }
+
+        void setHeight(int height) override{
+             this->width  = this->height = width;
+        }
+}
+
+
+//use factory to solve this problem
+struct RectangleFactory
+{
+    static Rectangle create_rectangle(int w, int h);
+    static Rectangle create_square(int size);
+}
+
+void process(Rectangle& r){
+    int w = r.getWidth();
+    r.setHeight(10);
+
+    std::cout << "expected area = " << (w*10) << " got " << r.area() << std::endl;
+}
+
+
+
+
